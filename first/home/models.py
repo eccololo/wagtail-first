64774b96d51f2ef2cbd891274c4819acd318caad
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2025-02-04 11:42:40 +0100

    Add fields do HomePage model for learning purposes.

--- first/home/models.py
+++ first/home/models.py
@@ -1,7 +1,59 @@
 from django.db import models
 
 from wagtail.models import Page
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel
+from wagtail.images.models import Image
+from wagtail.fields import RichTextField
+from wagtail.search import index
+from wagtail.fields import StreamField
+from wagtail import blocks
+from wagtail.images.blocks import ImageChooserBlock
 
 
 class HomePage(Page):
-    pass
+
+    call_to_action = models.CharField(max_length=255, blank=True, null=True)
+    # body = RichTextField(max_length=2000, blank=True, null=True)
+
+    body = StreamField([
+        ('heading', blocks.CharBlock(form_classname="title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+    ], use_json_field=True, default="")
+
+    date = models.DateField("Post date", blank=True, null=True)
+    header_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    feed_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    # Search index configuration
+    search_fields = Page.search_fields + [
+        index.SearchField('body'),
+        index.FilterField('date'),
+    ]
+
+
+    # Editor panels configuration
+    content_panels = Page.content_panels + [
+        FieldPanel('header_image'),
+        FieldPanel('date'),
+        FieldPanel('body'),
+        FieldPanel("call_to_action"),
+    ]
+
+    promote_panels = [
+        MultiFieldPanel(Page.promote_panels, "Common page configuration"),
+        FieldPanel('feed_image'),
+    ]
