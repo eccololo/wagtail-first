71f74b5944ee4a7a4731de6d58e8f8d5b312045f
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2025-02-04 10:07:46 +0100

    Add a start Wagtail project files.

---
+++ first/first/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-i2wmh&%sb*fw@3470=dem+rjsu5x3*+e%mr&ij%99$01fg#uhn"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
